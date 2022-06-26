f813ef3e3e283a549dac6eb18b0245de145b1f75
k1s3l3v <kiselev038@gmail.com>
2022-06-26 13:30:04 +0300

    Added logarithm, divide, multiply, pow of real and complex numbers, exponential output functions

--- complexNum.h
+++ complexNum.h
@@ -17,7 +17,13 @@ PyTypeObject cn_Type;
 complex_num* cn_init(double re, double im);
 complex_num* sum(complex_num cn1, complex_num cn2);
 complex_num* sub(complex_num cn1, complex_num cn2);
+complex_num* mul(complex_num cn1, complex_num cn2);
+complex_num* truediv(complex_num cn1, complex_num cn2);
+complex_num* r_pow(complex_num cn, double n);
+PyObject *cln(PyObject *self);
+PyObject *c_pow(PyObject *self1, PyObject *self2);
 complex_num* new_cn(PyObject* self);
 PyObject *print_alg(PyObject* self);
 PyObject *print_trig(PyObject* self);
+PyObject *print_exp(PyObject* self);
 #endif
